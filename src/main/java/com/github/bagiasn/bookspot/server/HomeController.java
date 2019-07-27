e19aee99c4b149bb29c1a9d3452b2fd9ea8feebd
Bojack <fixergaming@gmail.com>
2019-07-27 22:51:00 +0300

    Add home page
    Use semantic-ui as css framework

--- src/main/java/com/github/bagiasn/bookspot/server/HomeController.java
+++ src/main/java/com/github/bagiasn/bookspot/server/HomeController.java
@@ -1,12 +1,12 @@
 package com.github.bagiasn.bookspot.server;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/home")
     public String home() {
         return "Hello from BookSpot!";
     }
