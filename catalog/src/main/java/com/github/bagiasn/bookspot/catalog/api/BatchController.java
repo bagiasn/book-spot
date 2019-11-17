472d6856e2e91f97163ea97166d55417133ee689
bagiasn <fixergaming@gmail.com>
2019-11-17 16:53:11 +0200

    Add endpoint that supports batch insertions

---
+++ catalog/src/main/java/com/github/bagiasn/bookspot/catalog/api/BatchController.java
@@ -0,0 +1,39 @@
+package com.github.bagiasn.bookspot.catalog.api;
+
+import com.github.bagiasn.bookspot.common.models.Author;
+import com.github.bagiasn.bookspot.common.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog/batch")
+public class BatchController {
+
+    private  AuthorRepository authorRepository;
+    private  BookRepository bookRepository;
+
+    @Autowired
+    public BatchController(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping(value = "/authors", method = RequestMethod.POST)
+    public HttpStatus batchAuthorInsert(@RequestBody List<Author> authors) {
+
+        authorRepository.saveAll(authors);
+
+        return HttpStatus.OK;
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    public HttpStatus batchBookInsert(@RequestBody List<Book> books) {
+
+        bookRepository.saveAll(books);
+
+        return HttpStatus.OK;
+    }
+}
