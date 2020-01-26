4ac48c6e000cbdabfc032b5399ccb39b4e84c172
bagiasn <fixergaming@gmail.com>
2020-01-26 21:56:44 +0200

    Add users module

---
+++ user/src/main/java/com/github/bagiasn/bookspot/user/models/User.java
@@ -0,0 +1,74 @@
+package com.github.bagiasn.bookspot.user.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.bagiasn.bookspot.common.utils.HashGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private long id;
+    private String username;
+    @JsonProperty(value = "first_name")
+    private String firstName;
+    @JsonProperty(value = "last_name")
+    private String lastName;
+    private String email;
+    @JsonProperty(value = "password")
+    private String password;
+
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq_gen")
+    @SequenceGenerator(name = "users_seq_gen", sequenceName = "users_id_seq", initialValue = 1, allocationSize = 1)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = HashGenerator.GetPasswordHash(password);
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
