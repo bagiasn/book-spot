c19aa7af1555846ab6cd04fe1b6a2a8e7a814568
bagiasn <fixergaming@gmail.com>
2020-04-17 00:59:53 +0300

    Populate announcement with fake data
    Use faker and book titles from a local json

--- announcement/go.mod
+++ announcement/go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.3.3
-	github.com/sirupsen/logrus v1.5.0
-	google.golang.org/grpc v1.28.0
 	github.com/gorilla/websocket v1.4.2
+	github.com/sirupsen/logrus v1.5.0
+	github.com/bxcodec/faker/v3 v3.3.0
+	google.golang.org/grpc v1.28.0
 )
--- announcement/server/server.go
+++ announcement/server/server.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net"
 	"os"
 	"time"
 
 	pb "github.com/bagiasn/bookspot/announcement/api"
+	"github.com/bxcodec/faker/v3"
 
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
 var (
-	port = flag.Int("port", 12000, "The server's port")
+	port     = flag.Int("port", 12000, "The server's port")
+	jsonFile = flag.String("json_file", "./book_names.json", "The local json file to read book names from")
 )
+
+var bookData bookNames
+
+type bookNames struct {
+	Version int
+	Names   []string
+}
+
+type fakeAnnouncement struct {
+	Username string `faker:"first_name"`
+	Rating   int    `faker:"boundary_start=1, boundary_end=6"`
+	Delay    int    `faker:"boundary_start=10, boundary_end=20"`
+}
 
 type announcementServer struct {
 }
@@ -23,11 +41,22 @@ type announcementServer struct {
 func init() {
 	// Output to stdout instead of the default stderr
 	log.SetOutput(os.Stdout)
+
+	// Read book names from local json file.
+	data, err := ioutil.ReadFile(*jsonFile)
+	if err != nil {
+		log.Error("File reading error", err)
+	}
+
+	err = json.Unmarshal(data, &bookData)
+	if err != nil {
+		log.Error("Json deserialization error", err)
+	}
 }
 
 // Start a gRPC server that produces fake announcements.
 func main() {
-	log.Println("Staring Announcement Server.")
+	log.Info("Staring Announcement Server.")
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
@@ -50,10 +79,19 @@ func (s *announcementServer) Listen(in *pb.Request, stream pb.Announcement_Liste
 		"clientId": in.ClientId,
 	}).Info("Received client request")
 
+	data := fakeAnnouncement{}
+
 	for {
-		if err := stream.Send(&pb.AnnouncementMessage{Message: "Hello there chap"}); err != nil {
+		// Populate announcement with fake data.
+		_ = faker.FakeData(&data)
+		// Get a random int as book index.
+		index := rand.Intn(len(bookData.Names))
+		message := fmt.Sprintf("%v rated \"%v\" with %d", data.Username, bookData.Names[index], data.Rating)
+		// Send to client.
+		if err := stream.Send(&pb.AnnouncementMessage{Message: message}); err != nil {
 			return err
 		}
-		time.Sleep(5 * time.Second)
+
+		time.Sleep(time.Duration(data.Delay) * time.Second)
 	}
 }
