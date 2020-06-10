0b72a445a47fe7347c27007d123991b8088db963
bagiasn <fixergaming@gmail.com>
2020-06-11 01:19:39 +0300

    Mock internal dependencies

--- user/src/test/java/com/github/bagiasn/bookspot/user/UserControllerTests.java
+++ user/src/test/java/com/github/bagiasn/bookspot/user/UserControllerTests.java
@@ -1,21 +1,27 @@
 package com.github.bagiasn.bookspot.user;
 
+import com.github.bagiasn.bookspot.user.api.UserController;
+import com.github.bagiasn.bookspot.user.api.UserRepository;
 import com.github.bagiasn.bookspot.user.models.Credentials;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerTests {
 
+@WebMvcTest(UserController.class)
+public class UserControllerTests {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private StringRedisTemplate redisTemplate;
 
     @Test
     public void checkInputErrorReturnsBadRequest() throws Exception {
